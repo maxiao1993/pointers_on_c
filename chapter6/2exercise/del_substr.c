c1a4d076287c69559bea02bd0d60abba0a6bf261
maxiao1993 <soundwave_max@163.com>
2017-07-20 16:52:28 +0800

    modify del_substr.c

---
+++ chapter6/2exercise/del_substr.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int
+del_substr(char *str, char const *substr);
+
+char *
+match(char *str, char const *want);
+
+int 
+main()
+{
+	char str[] = "abcdefg";
+	char substr[] = "cdef";
+
+	del_substr(str,substr);
+
+	puts(str);
+	puts(substr);
+	return 0;
+}
+
+int
+del_substr(char *str, char const *substr)
+{
+	char *next;
+
+	while(*str != '\0')
+	{
+		next = match(str,substr);
+		if(next != NULL)
+		{
+			break;
+		}
+		str++;
+	}
+
+	if(*str == '\0')
+	{
+		return 0;
+	}
+
+	//想清楚这两个指针目前指向的位置。
+	while(*str++ = *next++)
+		;
+	return 1;
+}
+
+//判断两个字符串是否匹配。不用想太多，仅仅是判断两个字符串是否相等。
+char *
+match(char *str, char const *substr)
+{
+	while(*substr != '\0')
+	{
+		if(*str++ != *substr++)
+		{
+			return NULL;
+		}
+	}
+	return str;
+}
