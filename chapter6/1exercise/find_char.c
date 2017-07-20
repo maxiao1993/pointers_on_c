9f69dd3e4c81543a00819d9cd5a3eb341c86a395
maxiao1993 <soundwave_max@163.com>
2017-07-20 11:20:27 +0800

    add three files

---
+++ chapter6/1exercise/find_char.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+char *
+find_char( char const *source, char const *chars);
+
+int 
+main()
+{
+	char source[] = "abcdef";
+	char chars[] = "xrcqef";
+
+	char *a;
+	a = find_char(source,chars);
+	
+//	if(a == NULL)
+//		printf("a == NULL\n");
+//	else
+		printf("%c\n",*a);
+
+	return 0;
+}
+char *
+find_char( char const *source, char const *chars)
+{
+	
+//	if(source == NULL || chars == NULL)
+//	{
+//		printf("字符串不能为空\n");
+//		return NULL;
+//	}
+	char *ptr1;
+	char *ptr2;
+	for (ptr2 = chars ; *ptr2 != '\0'; ptr2++)
+	{
+		for(ptr1 = source; *ptr1 != '\0'; ptr1++)
+		{
+			if(*ptr1 == *ptr2)
+				return ptr1;
+		}
+	}
+
+	return NULL;
+}
